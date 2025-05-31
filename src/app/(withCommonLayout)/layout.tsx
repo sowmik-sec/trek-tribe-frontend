9819f6ad8fb43df011da056cfd29dc17d8734c07
sowmik-sec <sowmik.sec@gmail.com>
2025-05-31 22:51:18 +0600

    create layout

--- src/app/(withCommonLayout)/layout.tsx
+++ src/app/(withCommonLayout)/layout.tsx
@@ -1,5 +1,15 @@
+import Footer from "@/components/shared/Footer/Footer";
+import Navbar from "@/components/shared/Navbar/Navbar";
+import { Box } from "@mui/material";
+
 const CommonLayout = ({ children }: { children: React.ReactNode }) => {
-  return <>{children}</>;
+  return (
+    <>
+      <Navbar />
+      <Box sx={{ minHeight: "100vh" }}>{children}</Box>
+      <Footer />
+    </>
+  );
 };
 
 export default CommonLayout;
