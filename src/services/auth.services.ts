6fea25ec49f07a0159f8eba4e2ea361430cf765b
sowmik-sec <sowmik.sec@gmail.com>
2025-06-01 15:58:29 +0600

    create auth services

---
+++ src/services/auth.services.ts
@@ -0,0 +1,45 @@
+import { authKey } from "@/constants/authkey";
+import { instance as axiosInstance } from "@/helpers/axios/axiosInstance";
+import { decodedToken } from "@/utils/jwt";
+import {
+  getFromLocalStorage,
+  removeFromLocalStorage,
+  setToLocalStorage,
+} from "@/utils/local-sorage";
+
+export const storeUserInfo = ({ accessToken }: { accessToken: string }) => {
+  return setToLocalStorage(authKey, accessToken);
+};
+
+export const getUserInfo = () => {
+  const authToken = getFromLocalStorage(authKey);
+  if (authToken) {
+    const decodedData: any = decodedToken(authToken);
+    return {
+      ...decodedData,
+      role: decodedData?.role?.toLowerCase(),
+    };
+  } else {
+    return "";
+  }
+};
+
+export const isLoggedIn = () => {
+  const authToken = getFromLocalStorage(authKey);
+  if (authToken) {
+    return !!authToken;
+  }
+};
+
+export const removeUser = () => {
+  return removeFromLocalStorage(authKey);
+};
+
+export const getNewAccessToken = async () => {
+  return await axiosInstance({
+    url: "http://localhost:5000/api/v1/auth/refresh-token",
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    withCredentials: true,
+  });
+};
