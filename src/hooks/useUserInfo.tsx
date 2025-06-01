6856e2233e2fdb4af2e7f4973c18138875185968
sowmik-sec <sowmik.sec@gmail.com>
2025-06-01 15:59:44 +0600

    create useUserInfo hook

---
+++ src/hooks/useUserInfo.tsx
@@ -0,0 +1,32 @@
+import { authKey } from "@/constants/authkey";
+import { decodedToken } from "@/utils/jwt";
+import { getFromLocalStorage } from "@/utils/local-sorage";
+import { JwtPayload } from "jwt-decode";
+import { useEffect, useState } from "react";
+
+const useUserInfo = (): any | string => {
+  const [userInfo, setUserInfo] = useState<any | string>("");
+  useEffect(() => {
+    const fetchUserInfo = () => {
+      const authToken = getFromLocalStorage(authKey);
+      if (authToken) {
+        const decodedData: JwtPayload & { role: any } = decodedToken(
+          authToken
+        ) as JwtPayload & {
+          role: any;
+        };
+        const userInfo: any = {
+          ...decodedData,
+          role: decodedData.role?.toLowerCase() || "",
+        };
+        setUserInfo(userInfo);
+      } else {
+        setUserInfo(userInfo);
+      }
+    };
+    fetchUserInfo();
+  }, [userInfo]);
+  return userInfo;
+};
+
+export default useUserInfo;
