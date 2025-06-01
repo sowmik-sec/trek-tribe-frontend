1813667db22b60643fd9bc1af1ee04cc798e552b
sowmik-sec <sowmik.sec@gmail.com>
2025-06-01 15:59:21 +0600

    setup rtk query

---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,9 @@
+import { axiosBaseQuery } from "@/helpers/axios/axiosBaseQuery";
+import { createApi } from "@reduxjs/toolkit/query/react";
+
+export const baseApi = createApi({
+  reducerPath: "api",
+  baseQuery: axiosBaseQuery({ baseUrl: "http://localhost:5000/api/v1" }),
+  endpoints: () => ({}),
+  tagTypes: [],
+});
